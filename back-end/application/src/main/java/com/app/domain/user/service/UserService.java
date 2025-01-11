abf8a66c5eaf49345bfa9c69e00d6c27c7de2f39
xxl <3578144921@qq.com>
2025-01-11 16:46:26 +0800

    feat(登录):

--- back-end/application/src/main/java/com/app/domain/user/service/UserService.java
+++ back-end/application/src/main/java/com/app/domain/user/service/UserService.java
@@ -57,7 +57,14 @@ public class UserService extends AbstractService<UserMapper,UserEntity> {
     // 微信提供的API接口URL，需要替换为实际值
     private static final String WECHAT_LOGIN_URL = "https://api.weixin.qq.com/sns/jscode2session";
 
+    @Transactional(rollbackFor = RuntimeException.class)
     public UserEntity loginWithWechat(WeChatLoginParam param) {
+        UserEntity entity = new UserEntity();
+        entity.setPhoneNumber("15156246017");
+        entity.setNickname("test");
+        entity.setPwd("123456");
+        entity.setGender(1);
+        register(entity, true);
         return login("15156246017", "123456",true);
         /*final String resUrl = "%s?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code";
         String url = String.format(resUrl, WECHAT_LOGIN_URL, appid, secret, param.getCode());
