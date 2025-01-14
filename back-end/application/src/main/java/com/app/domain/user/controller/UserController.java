6ab6958fb981c372d3a4be452b8b37e3b1d8243f
james.aks <aksisnotx@gmail.com>
2025-01-14 15:22:20 +0800

    feat(用户接口增加提示):

--- back-end/application/src/main/java/com/app/domain/user/controller/UserController.java
+++ back-end/application/src/main/java/com/app/domain/user/controller/UserController.java
@@ -18,38 +18,38 @@ import org.springframework.web.bind.annotation.*;
  * @since 2024/3/16
  */
 
-@Tag(name = "[我的 & 登录] - 2025/01/12")
+@Tag(name = "[我的 & 登录] - 2025/01/14")
 @RequestMapping("/user")
 @RestController
 @Validated
 public class UserController extends Controller {
 
     @PostMapping("/auth/login/wechat")
-    @Operation(summary = "微信小程序登录")
+    @Operation(summary = "微信小程序登录 - [小程序]")
     public RespEntity<UserEntity> wechatLogin(@RequestBody @JsonView({Entity.LOGIN.class}) UserEntity param) {
         return RespEntity.success(userLoginService.loginWithWechat(param));
     }
 
     @PostMapping("/auth/register/wechat")
-    @Operation(summary = "微信小程序注册")
+    @Operation(summary = "微信小程序注册 - [小程序]")
     public RespEntity<?> wechatRegister(@RequestBody @JsonView({Entity.INSERT.class}) @Validated(Entity.INSERT.class) UserEntity param) {
         return RespEntity.success(userLoginService.registerWithWechat(param));
     }
 
     @PostMapping("/auth/login")
-    @Operation(summary = "后台登录")
+    @Operation(summary = "后台登录 - [后台]")
     public RespEntity<UserEntity> login(@RequestBody @JsonView({Entity.ADMIN_LOGIN.class}) @Validated(Entity.ADMIN_LOGIN.class) UserEntity param) {
         return RespEntity.success(userLoginService.login(param));
     }
 
     @PostMapping("/modifyUserInfo")
-    @Operation(summary = "修改个人信息")
+    @Operation(summary = "修改个人信息 - [后台 & 小程序]")
     public RespEntity<UserEntity> modifyUserInfo(@RequestBody @JsonView({Entity.UPDATE.class}) @Validated(Entity.UPDATE.class) UserEntity param) {
         return RespEntity.success(userLoginService.modifyUserInfo(param));
     }
 
     @GetMapping("/get")
-    @Operation(summary = "获取个人信息")
+    @Operation(summary = "获取个人信息 - [后台 & 小程序]")
     public RespEntity<UserEntity> get() {
         return RespEntity.success(LoginUser.getLoginUser());
     }
@@ -57,21 +57,21 @@ public class UserController extends Controller {
 
     //查看用户列表
     @GetMapping("/list")
-    @Operation(summary = "查看用户列表")
+    @Operation(summary = "查看用户列表 - [后台]")
     public RespEntity<Page<UserEntity>> list() {
         return RespEntity.success(userLoginService.lambdaQuery().page(CommonPageRequestUtils.defaultPage()));
     }
 
     //查看用户详情信息
     @GetMapping("/info/{id}")
-    @Operation(summary = "查看用户信息")
+    @Operation(summary = "查看用户信息 - [后台 & 小程序]")
     public RespEntity<UserEntity> info(@PathVariable String id) {
         return RespEntity.success(userLoginService.getById(id));
     }
 
     //删除用户
     @GetMapping("/delete/{id}")
-    @Operation(summary = "删除用户")
+    @Operation(summary = "删除用户 - [后台]")
     public RespEntity<Boolean> delete(@PathVariable String id) {
         return RespEntity.success(userLoginService.deleteUserById(id));
     }
